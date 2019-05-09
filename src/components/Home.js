6e219babb81ad3401d10610b9490541cb96069c1
Maris Herrera <maris.lizete.herrera@gmail.com>
2019-05-09 02:55:46 -0600

    adding mobil features for about page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,114 +1,127 @@
 import React from "react";
-import trees from "../images/trees.png";
+import trees from "../images/paint.png";
 import { Container, Header, Image, Icon, Divider, } from "semantic-ui-react";
 import '../styles/homeStyles.css';
+import pfp from "../images/pfppink1.jpg";
 
 const Home = () => (
-  <div id='background'>
+  <div>
+
+    <div id="headerColor1">
+      <Image className="img1" src={pfp} size='medium' circular />
+      <div id="circle-container1">
+        <div className="position1 circle1"></div>
+      </div>
+      <p id="name1"as='h1'>Maris <br /> Herrera</p>
+      <p id="smallHeader1">Artist Designer Coder </p>
+    </div>
     <Image id='homeBanner' src={trees}/>
-    <Header id="servicesFont">S E R V I C E S</Header>
-    <div id="row">
-      <div className="block">
-        <Icon className='serviceIcon' size="huge" name="paint brush"/>
+    
+    <div id='background'>
+      <Header id="servicesFont">S E R V I C E S</Header>
+      <div id="row">
+        <div className="block">
+          <Icon className='serviceIcon' size="huge" name="paint brush"/>
+        </div>
+        <div className="block">
+          <Icon className='serviceIcon' size="huge" name="code"/>
+        </div>
+        <div className="block">
+          <Icon className='serviceIcon' size="huge" name="pencil"/>
+        </div>
+        <div className="block">
+          <Icon className='serviceIcon' size="huge" name="cubes"/>
+        </div>
+        <div className="block">
+          <Icon className='serviceIcon' size="huge" name="language"/>
+        </div>       
       </div>
-      <div className="block">
-        <Icon className='serviceIcon' size="huge" name="code"/>
-      </div>
-      <div className="block">
-        <Icon className='serviceIcon' size="huge" name="pencil"/>
-      </div>
-      <div className="block">
-        <Icon className='serviceIcon' size="huge" name="cubes"/>
-      </div>
-      <div className="block">
-        <Icon className='serviceIcon' size="huge" name="language"/>
-      </div>       
-    </div>
 
-    <div id="textRow">
-      <div className="text">
-        <p >Art</p>
+      <div id="textRow">
+        <div className="text">
+          <p >Art</p>
+        </div>
+        <div className="text">
+          <p>Full-stack web development</p>
+        </div>
+        <div className="text">
+          <p>Digital Media</p>
+        </div>
+        <div className="text">
+          <p>Game Design <br /> & 3d Modeling</p>
+        </div>
+        <div className="text">
+          <p>Language</p>
+        </div>
       </div>
-      <div className="text">
-        <p>Full-stack web development</p>
-      </div>
-      <div className="text">
-        <p>Digital Media</p>
-      </div>
-      <div className="text">
-        <p>Game Design <br /> & 3d Modeling</p>
-      </div>
-      <div className="text">
-        <p>Language</p>
-      </div>
-    </div>
-    <br />
-    <Divider id='line'/>
+      <br />
+      <Divider id='line'/>
 
-    <div id="bottomRow">
-      <ul className="bottomText">
-        <li>Traditional</li>
-        <li>Digital</li>
-        <li>Mixed Media</li>
-        <li>Hand Drawn Lightbox Animation</li>
-      </ul>
-      <ul className="bottomText">
-        <li>React Nativel</li>
-        <li>Ruby on rails</li>
-        {/* <li>Github</li> */}
-        {/* <li>JSON</li> */}
-        <li>JavaScript</li>
-        {/* <li>Redux.js</li> */}
-        <li>CSS</li>
-        <li>HTML5</li>
-        {/* <li>jQuery</li> */}
-        {/* <li>Git</li> */}
-        <li>SQL</li>
-        {/* <li>Node.js</li> */}
-        {/* <li>HTML</li> */}
-        <li>etc.(portfolio)</li>
-      </ul>
-      <ul className="bottomText">
-        <li>Adobe Photoshop</li>
-        <li>Adobe Illustrator</li>
-      </ul>
-      <ul className="bottomText">
-        <li>3D Studio Max</li>
-        <li>Autodesk Maya</li>
-        <li>Zbrush</li>
-      </ul>
-      <ul className="bottomText">
-        <li>Spanish</li>
-        <li>English</li>
-      </ul>
-    </div>
+      <div id="bottomRow">
+        <ul className="bottomText">
+          <li>Traditional</li>
+          <li>Digital</li>
+          <li>Mixed Media</li>
+          <li>Hand Drawn Lightbox Animation</li>
+        </ul>
+        <ul className="bottomText">
+          <li>React Nativel</li>
+          <li>Ruby on rails</li>
+          {/* <li>Github</li> */}
+          {/* <li>JSON</li> */}
+          <li>JavaScript</li>
+          {/* <li>Redux.js</li> */}
+          <li>CSS</li>
+          <li>HTML5</li>
+          {/* <li>jQuery</li> */}
+          {/* <li>Git</li> */}
+          <li>SQL</li>
+          {/* <li>Node.js</li> */}
+          {/* <li>HTML</li> */}
+          <li>etc.(portfolio)</li>
+        </ul>
+        <ul className="bottomText">
+          <li>Adobe Photoshop</li>
+          <li>Adobe Illustrator</li>
+        </ul>
+        <ul className="bottomText">
+          <li>3D Studio Max</li>
+          <li>Autodesk Maya</li>
+          <li>Zbrush</li>
+        </ul>
+        <ul className="bottomText">
+          <li>Spanish</li>
+          <li>English</li>
+        </ul>
+      </div>
 
-    <div id='basics'>
-      <Container textAlign='left'><Header> Left Aligned </Header></Container>
-      <Container textAlign='center'><Header> Center Aligned </Header></Container>
-      <Container textAlign='right'><Header> Right Aligned </Header></Container>
-      <Container textAlign='justified'>
-        <Header as='h1'>Basics</Header>
-        <Divider />
-        <p className="basicsParagraph">
-          My name is Maris Herrera
-          I'm a web developer and Artist based in Layton, Utah.
-          Some of my hobbies include cosplaying, playing horror games, drawing, painting, and doing anything
-          artsy/crafty. I love learning new skills to challenge myself! Im a pretty outgoing person and I
-          don't have any issues talking with big groups of people or crowds.
-          When it comes to web development I can do both front and back end. design or develope either way is
-          fine with me.
-          Im a very social person and i consider that a skill
-          I speak both english and spanish
+      {/* <div id='basics'>
+        <Container textAlign='left'><Header> Left Aligned </Header></Container>
+        <Container textAlign='center'><Header> Center Aligned </Header></Container>
+        <Container textAlign='right'><Header> Right Aligned </Header></Container>
+        <Container textAlign='justified'>
+          <Header as='h1'>Basics</Header>
+          <Divider />
+          <p className="basicsParagraph">
+            My name is Maris Herrera
+            I'm a web developer and Artist based in Layton, Utah.
+            Some of my hobbies include cosplaying, playing horror games, drawing, painting, and doing anything
+            artsy/crafty. I love learning new skills to challenge myself! Im a pretty outgoing person and I
+            don't have any issues talking with big groups of people or crowds.
+            When it comes to web development I can do both front and back end. design or develope either way is
+            fine with me.
+            Im a very social person and i consider that a skill
+            I speak both english and spanish
 
-          some fun facts about me
-          I don't have a favorite color, I have 4 because im indesisive. I also have favorite color pallets
-          my favorite season is fall because I love fall colors and i love seeing the red leaves on the ground
-          It makes everywhere look so gorgeous
-          I listen to all kinds of music! I absolutely love music but the genres I listen to the most are
-          pop-punk, indie, and Lo-fi.
-        </p>
-      </Container>
+            some fun facts about me
+            I don't have a favorite color, I have 4 because im indesisive. I also have favorite color pallets
+            my favorite season is fall because I love fall colors and i love seeing the red leaves on the ground
+            It makes everywhere look so gorgeous
+            I listen to all kinds of music! I absolutely love music but the genres I listen to the most are
+            pop-punk, indie, and Lo-fi.
+          </p>
+        </Container>
+      </div> */}
     </div>
   </div>
 );
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,44 +1,191 @@
 import React from 'react';
-import { Header, Image, Segment, Icon, } from "semantic-ui-react";
-import { Link, } from 'react-router-dom';
+import { Icon, } from "semantic-ui-react";
+// import { Link, } from 'react-router-dom';
 import tableapp from "../images/tableapp.png";
+import zerotwo from "../images/artPics/02.jpg";
+import edge from "../images/artPics/edge.jpg";
+import emo from "../images/artPics/emo.jpg";
+import gold from "../images/artPics/gold.jpg";
+import hand from "../images/artPics/hand.jpg";
+import pink from "../images/artPics/pink.png";
+import tattoo from "../images/artPics/tattoo.jpg";
+import vamp from "../images/artPics/vamp.jpg";
+//seperate images
+import chibi from "../images/3dPics/chibi.jpg";
+import girl from "../images/3dPics/girl.png";
+import house from "../images/3dPics/house.jpg";
+import monkey from "../images/3dPics/monkey.png";
+import train from "../images/3dPics/train.png";
 import '../styles/portfolioStyles.css';
+import { Slide } from 'react-slideshow-image';
 
-const Portfolio = () => (
-  <div>
+const artSlideImages = [
+  zerotwo,
+  edge,
+  emo,
+  gold,
+  hand,
+  pink,
+  tattoo,
+  vamp
+];
+
+const threeDSlideImages = [
+  chibi,
+  girl,
+  house,
+  monkey,
+  train
+];
+
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicatiors: true,
+  arrows: true
+}
+
+const Portfolio = () => {
+  return(
+  <div className="portfolioWrap">
     <p id="portfolioFont">P O R T F O L I O</p>
     <p id="name"as='h1' id="ppHeader">&<br />Personal Projects</p>
-    {/* <div id="circle-container2">
-        <div className="position2 circle2 shadow2s"></div>
-    </div> */}
-    
+
     <div className="colorBlock">
-      <p className="projectTitle" as='h2' floated='left'>
+      <p id="taTitle" as='h2' floated='left'>
         TABLE-APP
       </p>
+      <div className="dottedLine"/>
       <div className="underLine"/>
-      <Link to="/">
-        <Image id="portfolioPic" src={tableapp}/>
-      </Link>
-      <p className="projectDesc">
-        Table-App is an Application that would allow customers<br />
-        an easier way to find and reserve tables at a nightclub.<br />
-        I worked on a team of five developers to architect and<br />
-        create the app. Technologies used include a custom Ruby<br />
+      <a href="https://w18-table-app.herokuapp.com/">
+        <img id="tableappPic" src={tableapp}/>
+      </a>
+      <p id="projectDesc">
+        Table-App is an Application that would allow customers
+        an easier way to find and reserve tables at a nightclub.
+        I worked on a team of five developers to architect and
+        create the app. Technologies used include a custom Ruby
         on Rails Backend API Server, and React JS Client Side.
       </p>
-      <p className="projectLinkText">
-        CHECK OUT TABLE APP 
-        <Icon id="arrow" name='long arrow alternate right' size='small' />
+      <p id="projectSText">
+        <Icon className="heart" name='heart' size='large' />
+        CHECK OUT TABLE-APP 
+        <Icon className="heart" name='heart' size='large' />
       </p>
-      <div className="underLine2"/>
-      <p id="projectLink">
-        CLICK ME
+      <div id="blueLine"/>
+      <p id="projectLText">
+        CLICK IMAGE
       </p>
     </div>
+
     <br />
     <br />
+
+
+
+
+    <div className="colorBlock">
+      <p id="artTitle" as='h2' floated='left'>
+        ART
+      </p>
+      <div className="dottedLine"/>
+      <div className="underLine"/>
+
+      <Slide className="slide-cont" {...properties}>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${artSlideImages[0]})`}}/>
+        </div>
+
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${artSlideImages[1]})`}}/>
+        </div>
+
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${artSlideImages[2]})`}}/>
+        </div>
+
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${artSlideImages[3]})`}}/>
+        </div>
+
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${artSlideImages[4]})`}}/>
+        </div>
+
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${artSlideImages[5]})`}}/>
+        </div>
+
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${artSlideImages[6]})`}}/>
+        </div>
+
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${artSlideImages[7]})`}}/>
+        </div> 
+      </Slide>
+      <p id="projectSText2">
+        <Icon className="heart" name='heart' size='big' />
+        IF YOU WOULD LIKE TO SEE MORE ART, CHECK OUT MY 
+        <Icon className="heart" name='heart' size='big' />
+      </p>
+      <div id="blueLine2"/>
+      <a href="https://www.instagram.com/nyanimania/?hl=en">
+        <p id="projectLText2">
+          INSTAGRAM
+        </p>
+      </a>
+    </div>
+
+
+    <br />
+    <br />
+    <div id="colorBlock3">
+      <p id="threeDTitle" as='h2' floated='left'>
+        3D Modeling
+      </p>
+      <div className="dottedLine"/>
+      <div className="underLine"/>
+
+      <Slide className="slide-cont" {...properties}>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${threeDSlideImages[0]})`}}/>
+        </div>
+
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${threeDSlideImages[1]})`}}/>
+        </div>
+
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${threeDSlideImages[2]})`}}/>
+        </div>
+
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${threeDSlideImages[3]})`}}/>
+        </div>
+
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${threeDSlideImages[4]})`}}/>
+        </div>     
+      </Slide>
+      
+      <p id="projectDesc3">
+      I have taken courses and learned how to use programs such
+      as but not limited to: Autodesk 3ds Max, Autodesk Maya,
+      ZBrush, GameMaker Studio 2, Adobe Photoshop, and Adobe
+      Illustrator. In addition, I have taken Storyboard and Animation
+      courses. While I attended High School at AISU (American International
+      School of Utah), I also took related tech classes and learned the 
+      basics of Microsoft Excel and Autodesk AutoCAD.
+
+      </p>
+    </div>
+
   </div>
-)
+
+  )
+}
 
 export default Portfolio;
